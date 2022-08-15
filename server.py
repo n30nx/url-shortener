80845ab9bb8ad734c7d34d00b8d21375e3280ac2
n30nx <22144985+n30nx@users.noreply.github.com>
2022-08-15 16:30:32 +0300

    Update server.py

--- server.py
+++ server.py
@@ -9,21 +9,17 @@ db = SQLAlchemy(app)
 
 
 class shortener(db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
     url_short = db.Column(db.String(300), nullable=False)
     redirect_url = db.Column(db.String(300), nullable=False)
 
     def __repr__(self):
-
         return '<url %r>' % self.id
 
 
-def short():
-
+def short() -> str:
+    global alphabet
     return_val = ""
-
-    alphabet = [chr(i) for i in range(97, 123)]
 
     while len(return_val) < 6:
         rand = random.randint(0, 100)
@@ -37,7 +33,6 @@ def short():
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-
     urls = shortener.query.order_by(shortener.url_short).all()
     short_url = flask.request.args.get('s')
 
@@ -57,8 +52,8 @@ def main():
             url_content = flask.request.form['content']
             while True:
                 s = short()
-                q = shortener.query.filter_by(url_short=s).first()
-                if q is None:
+                qeury = shortener.query.filter_by(url_short=s).first()
+                if query is None:
                     break
                 else:
                     continue
